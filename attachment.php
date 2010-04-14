ff89707b3e82c00c9a258a4cc02f81dc9a1d2a28
flyinghail <flyinghail@msn.com>
2010-04-14 10:23:39 +0000

    Head 修改

--- attachment.php
+++ attachment.php
@@ -2,14 +2,9 @@
 # **************************************************************************#
 # MolyX2
 # ------------------------------------------------------
-# copyright (c) 2004-2006 HOGE Software.
-# official forum : http://molyx.com
-# license : MolyX License, http://molyx.com/license
-# MolyX2 is free software. You can redistribute this file and/or modify
-# it under the terms of MolyX License. If you do not accept the Terms
-# and Conditions stated in MolyX License, please do not redistribute
-# this file.Please visit http://molyx.com/license periodically to review
-# the Terms and Conditions, or contact HOGE Software.
+# @copyright (c) 2009-2010 MolyX Group..
+# @official forum http://molyx.com
+# @license http://opensource.org/licenses/gpl-2.0.php GNU Public License 2.0
 #
 # $Id$
 # **************************************************************************#
--- includes/page_cache.php
+++ includes/page_cache.php
@@ -2,20 +2,15 @@
 # **************************************************************************#
 # MolyX2
 # ------------------------------------------------------
-# copyright (c) 2004-2006 HOGE Software.
-# official forum : http://molyx.com
-# license : MolyX License, http://molyx.com/license
-# MolyX2 is free software. You can redistribute this file and/or modify
-# it under the terms of MolyX License. If you do not accept the Terms
-# and Conditions stated in MolyX License, please do not redistribute
-# this file.Please visit http://molyx.com/license periodically to review
-# the Terms and Conditions, or contact HOGE Software.
+# @copyright (c) 2009-2010 MolyX Group..
+# @official forum http://molyx.com
+# @license http://opensource.org/licenses/gpl-2.0.php GNU Public License 2.0
 #
 # $Id$
 # **************************************************************************#
 $config = array(
-	'index' => 3600, //index.php ¹ýÆÚÊ±¼ä
-	'showthread' => 3600, //showthread.php ¹ýÆÚÊ±¼ä
+	'index' => 3600, //index.php ï¿½ï¿½ï¿½ï¿½Ê±ï¿½ï¿½
+	'showthread' => 3600, //showthread.php ï¿½ï¿½ï¿½ï¿½Ê±ï¿½ï¿½
 );
 
 if (array_key_exists(THIS_SCRIPT, $config))
